b1fee9240fc8ccce6725a20b026cca9c39d72091
Michal Mastej <m.mastej1812@gmail.com>
2025-03-27 17:40:11 +0100

    Creating raw CNN model

--- main.py
+++ main.py
@@ -64,4 +64,39 @@ def prepare_data():
     )
     return x_train, x_val, y_train, y_val
 
-prepare_data()
+IMG_SIZE = 256
+model = keras.models.Sequential([
+    layers.Conv2D(filters=32,
+                  kernel_size=(5, 5),
+                  activation='relu',
+                  input_shape=(IMG_SIZE,
+                               IMG_SIZE,
+                               3),
+                  padding='same'),
+    layers.MaxPooling2D(2, 2),
+
+    layers.Conv2D(filters=64,
+                  kernel_size=(3, 3),
+                  activation='relu',
+                  padding='same'),
+
+    layers.MaxPooling2D(2, 2),
+
+    layers.Conv2D(filters=128,
+                  kernel_size=(3, 3),
+                  activation='relu',
+                  padding='same'),
+
+    layers.MaxPooling2D(2, 2),
+
+    layers.Flatten(),
+    layers.Dense(256, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.3),
+    layers.BatchNormalization(),
+    layers.Dense(3, activation='softmax')
+])
+model.summary()
+
+# prepare_data()
